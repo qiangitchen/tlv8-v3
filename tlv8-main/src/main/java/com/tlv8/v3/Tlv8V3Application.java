fc019c3a1058ae43e268b9553f04c278be3fd42a
陈乾 <qianpou@163.com>
2024-11-29 13:45:10 +0800

    基础

---
+++ tlv8-main/src/main/java/com/tlv8/v3/Tlv8V3Application.java
@@ -0,0 +1,13 @@
+package com.tlv8.v3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tlv8V3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tlv8V3Application.class, args);
+	}
+
+}
---
+++ tlv8-main/src/test/java/com/tlv8/v3/Tlv8V3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tlv8.v3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tlv8V3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
